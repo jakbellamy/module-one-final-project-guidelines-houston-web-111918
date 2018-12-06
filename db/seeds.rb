d5d46bcdb3dcd0822ffa8f2408036427a14fc0e7
github username <github email address>
2018-12-06 10:35:16 -0600

    still dont have the locations looking right in reports but we have the zip here and it works

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,58 +19,20 @@ seed_locations
   
 def seed_crimes
   hpd_log.each do |incident|
-    all_incident_type = incident["incident_type_primary"]
+    all_incident_type = incident["incident_type_primary"][4...50]
     all_parent_incident = incident["parent_incident_type"]
     all_date = incident["incident_datetime"][0...-13]
-  
     new_crime = Crime.new(incident_type: all_incident_type, parent_incident: all_parent_incident)
     new_crime.save
 
-    full_incident = all_parent_incident + ": " + all_incident_type
+    full_incident = "#{all_parent_incident} (#{all_incident_type})"
 
-    location_found = (Location.where(city: incident["city"]).where_values_hash).values
-
-    # location_fixed = location_found.each do |location| JSON.parse(location.self.location) end
-    
+    city_found = (Location.where(city: incident["city"]).where_values_hash).values
+    zip_found = (Location.where(zip_code: incident["zip"]).where_values_hash).values
+    location_found = "#{zip_found}, #{city_found}"
+    # location_fixed = city_found.each do |location| JSON.parse(location.self.location) end
     Report.create(crime: full_incident, location: location_found, date: all_date)
-
-    
   end
 end
 
 seed_crimes
-
-
-
-
-# def seed_reports
-#   hpd_log.each do |incident|
-#     all_date = incident["incident_datetime"]
-
-#   new_report = Report.new(crime: seed_crimes, location: seed_locations, date:  all_date)
-#   # new_report.save
-#   end
-# end
-
-# seed_reports
-
-
-
-
-# def crimes
-#   hpd_log.map do |incident|
-#     Crime.each(incident_type: incident[:incident_type:],parent_incident: incident[:parent_incident_type])
-#   end
-# end
-
-# def reports
-#   Report.create()
-# end
-
-# hpd_log
-# locations
-# crimes
-# reports
-
-# binding.pry
-# 0
