faa28b2254d57cf37f2a0b21527e67840846626e
github username <github email address>
2018-12-06 11:40:12 -0600

    first run method created, others outlined

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,41 @@
 require_relative '../config/environment'
 
+##RETURN CRIMES FOR GIVEN ZIP
+def output(a)
+    a.each do |val| puts "#{val.crime} || #{val.date}" end
+        return nil
+end
+
+def search_by_zip
+  puts "what's your zipcode?"
+  zip = gets.chomp
+  x = Report.all.select{|i| i.location.include?("#{zip}")} 
+  output(x)
+end
+search_by_zip
+#-----------------------------------------------------------#
+##RETURN MOST COMMON CRIME(IN TOTAL)
 
 
-puts "HELLO WORLD"
+
+##RETURN MOST COMMON CRIME FOR GIVEN ZIP
+
+
+#-----------------------------------------------------------#
+##RETURN RANK ZIPS BY CRIME INSTANCES
+
+
+
+#-----------------------------------------------------------#
+##WHERE AM I MOST LIKELY TO GET
+  
+  #MURDERED
+  
+  #STOLEN FROM
+
+  #MY PROPERTY DAMAGED
+
+
+  
+  
 
