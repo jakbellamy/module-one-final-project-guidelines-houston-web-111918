c47c9f9ea1938887b354d80959ba544af714ee1a
github username <github email address>
2018-12-06 07:46:05 -0600

    moreadf

--- db/migrate/20181204180330_reports.rb
+++ db/migrate/20181204180330_reports.rb
@@ -1,8 +1,8 @@
 class Reports < ActiveRecord::Migration[5.0]
   def change
     create_table :reports do |t|
-      t.belongs_to :crime
-      t.belongs_to :location
+      t.string :crime
+      t.string :location
       t.string :date
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
 require_relative '../config/environment.rb'
-
-
-# Location.destroy_all
-# Crime.destroy_all
-# Report.destroy_all
-
 def hpd_log
     response_string = JSON.parse(RestClient.get("https://moto.data.socrata.com/resource/p6kq-vsa3.json"))
 end
@@ -27,19 +21,38 @@ def seed_crimes
   hpd_log.each do |incident|
     all_incident_type = incident["incident_type_primary"]
     all_parent_incident = incident["parent_incident_type"]
-
+    all_date = incident["incident_datetime"][0...-13]
+  
     new_crime = Crime.new(incident_type: all_incident_type, parent_incident: all_parent_incident)
     new_crime.save
+
+    full_incident = all_parent_incident + ": " + all_incident_type
+
+    location_found = (Location.where(city: incident["city"]).where_values_hash).values
+
+    # location_fixed = location_found.each do |location| JSON.parse(location.self.location) end
+    
+    Report.create(crime: full_incident, location: location_found, date: all_date)
+
+    
   end
 end
 
 seed_crimes
 
 
-def reports
-  hpd_log.each do |incident|
-    all_date = incident["incident_datetime"]
-  end
+
+
+# def seed_reports
+#   hpd_log.each do |incident|
+#     all_date = incident["incident_datetime"]
+
+#   new_report = Report.new(crime: seed_crimes, location: seed_locations, date:  all_date)
+#   # new_report.save
+#   end
+# end
+
+# seed_reports
 
 
 
@@ -58,3 +71,6 @@ def reports
 # locations
 # crimes
 # reports
+
+# binding.pry
+# 0
